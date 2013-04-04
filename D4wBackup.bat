16c2fce273fa2bcbac4441b68888b99a66a56b30
Soongwoo Chong <soongwoo@gmail.com>
2013-04-05 04:46:16 +1100

    Add D4wBackup.bat
    Create 7z backup file.
    Can't add 7z file size.

---
+++ D4wBackup.bat
@@ -0,0 +1,26 @@
+@ECHO OFF
+SET EXE7Z=C:\Windows\System32\7za.exe
+REM SET LOG=E:\Dropbox\Today.D4w\d4wbackup.log
+SET LOG=d4wbackup.log
+SET SOURCE=E:\D4w_Data\D4w.*
+SET TARGET=E:\Dropbox\Today.D4w\ST-d4w.7z
+
+ECHO==================================== >> %LOG%
+echo Start: %date% %time% >> %log%
+
+IF NOT EXIST %EXE7Z% GOTO NOEXE7Z
+
+IF NOT EXIST %TARGET% GOTO RUN7Z
+del %TARGET%
+
+:RUN7Z
+%EXE7Z% a -t7z %TARGET% %SOURCE%
+REM for %I in (E:\Dropbox\Today.D4w\ST-d4w.7z) do @echo %~znI >> %log%
+GOTO ENDBATCH
+
+:NOEXE7Z
+echo NO %EXE7Z% >> %log%
+
+:ENDBATCH
+echo Finish: %date% %time% >> %log%
+ECHO==================================== >> %LOG%
