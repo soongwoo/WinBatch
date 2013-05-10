a7038ff0772ca8105d7bdfb4821de20a20a572b1
Soongwoo Chong <soongwoo@gmail.com>
2013-05-10 18:06:22 +1000

    add euckr2utf6.bat
    Convert the given file's encoding from euc-kr to utf-8.

--- D4wBackup.bat
+++ D4wBackup.bat
@@ -1,26 +1,19 @@
 @ECHO OFF
-SET EXE7Z=C:\Windows\System32\7za.exe
-:: SET LOG=E:\Dropbox\Today.D4w\d4wbackup.log
-SET SET LOG=d4wbackup.log
-SET SOURCE=E:\D4w_Data\D4w.*
-SET TARGET=E:\Dropbox\Today.D4w\ST-d4w.7z
+::
+:: Create 7zip backup file
+::
 
-ECHO==================================== >> %LOG%
-echo Start: %date% %time% >> %log%
+set Month=%date:~3,2%
+set Day=%date:~0,2%
+set Year=%date:~6,4%
 
-IF NOT EXIST %EXE7Z% GOTO NOEXE7Z
+:: set Branch=ST
+set Branch=CW
+set BackupFilename=%Branch%%Year%%Month%%Day%.7z
 
-IF NOT EXIST %TARGET% GOTO RUN7Z
-del %TARGET%
+echo "Create %BackupFilename% at %date% %time%"
 
-:RUN7Z
-%EXE7Z% a -t7z %TARGET% %SOURCE%
-:: for %I in (E:\Dropbox\Today.D4w\ST-d4w.7z) do @echo %~znI >> %log%
-GOTO ENDBATCH
+:: C:\Program Files\7-Zip\ should be in %PATH%
+7z a ..\Backup\%BackupFilename% D4w.*
 
-:NOEXE7Z
-echo NO %EXE7Z% >> %log%
-
-:ENDBATCH
-echo Finish: %date% %time% >> %log%
-ECHO==================================== >> %LOG%
+echo "Finish %BackupFilename% at %date% %time%"
---
+++ euckr2utf8.bat
@@ -0,0 +1,31 @@
+@echo off
+::
+:: Convert document's encoding from euc-kr to utf-8.
+::
+
+:: variables
+set MV=ren
+set RM=del
+set TMPFILE=tmpfile
+
+:: Check whether it is encoded with euc-kr.
+
+:: reserve the output name
+set OUTFILE=%1
+
+:: check whether it is a valid argument
+IF NOT EXIST %OUTFILE% (
+  echo '%OUTFILE%' does not exist!
+  goto END
+)
+
+: rename the argument to temporary one
+%RENAME% %1 %TMPFILE%
+
+:: convert it with iconv windows binary
+echo Convert %OUTFILE%'s encoding to utf-8 ...
+iconv -f euc-kr -t utf-8 %TMPFILE% > %OUTFILE%
+%RM% %TMPFILE%
+
+:END
+
